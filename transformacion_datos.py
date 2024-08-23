8194ce62eb45ac037664ffbb16a0dc57fe5e2427
jdgarcia3 <jgarcia3@unisangil.edu.co>
2024-08-23 16:50:53 -0500

    Se agrego un comentarioe

--- transformacion_datos.py
+++ transformacion_datos.py
@@ -4,3 +4,4 @@ cedula = int(input("Digite su cedula: "))
 estatura = float(input("Digite su estatura: "))
 estado = bool(input("Digite su estado: "))
 print(f"Su nombre es {nombre}, su cedula es {cedula}, su estatura es {estatura} y su estado es {estado}")
+# Actualización github
