7d6458b21bfa24466a835df110b40aa248aa5de8
David García <jgarcia3@unisangil.edu.co>
2024-08-23 16:48:24 -0500

    transformacion varibles, captura de datos y mostrar datos

---
+++ transformacion_datos.py
@@ -0,0 +1,6 @@
+#capturar informacion 
+nombre = input("Digite su nombre completo: ")
+cedula = int(input("Digite su cedula: "))
+estatura = float(input("Digite su estatura: "))
+estado = bool(input("Digite su estado: "))
+print(f"Su nombre es {nombre}, su cedula es {cedula}, su estatura es {estatura} y su estado es {estado}")
