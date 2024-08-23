a14b5a998976084f09f444c42c9be97545f6e2bc
David García <jgarcia3@unisangil.edu.co>
2024-08-23 16:22:11 -0500

    tipos de variables: numeros, strig, booleanos

---
+++ tipos_variables.py
@@ -0,0 +1,11 @@
+#tipos de variable 
+cadena = "github"
+print(type(cadena))
+numero = 10
+print(type(numero))
+decimal = 11.190
+print(type(decimal))
+boolean = True
+print(type(boolean))
+letra = "a"
+print(type(letra))
