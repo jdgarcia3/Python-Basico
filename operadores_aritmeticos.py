2d7c4f984e839cbb4575b2c25933076092dd71d6
jdgarcia3 <jgarcia3@unisangil.edu.co>
2024-08-30 15:36:44 -0500

    Ejemplo 3

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -9,3 +9,8 @@ ejemplo1 = 5*(2+5)
 ejemplo2 = (4+3)-((3*2)+1)
 #Imprimir variable
 print(f"El resultado de la operacion es: {ejemplo2}")
+
+#Ejemplo 3
+ejemplo3 = 2*3+5(9/3)
+#Imprimir variable
+print(f"El resultado de la operacion es: {ejemplo3}")
