eb1a28d76148fc62eb34fdd593ae2ab7570621f9
David García <jgarcia3@unisangil.edu.co>
2024-08-30 15:28:17 -0500

    Ejercicio 2

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -3,4 +3,9 @@ import math
 #Ejemplo 1
 ejemplo1 = 5*(2+5)
 #Imprimir variable
-print(f"El resultado de laoperacion es: {ejemplo1}")
+#print(f"El resultado de la operacion es: {ejemplo1}")
+
+#Ejemplo 2
+ejemplo2 = (4+3)-((3*2)+1)
+#Imprimir variable
+print(f"El resultado de la operacion es: {ejemplo2}")
