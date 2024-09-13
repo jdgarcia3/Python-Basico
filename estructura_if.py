92c2dafdd40866f5bf9afead9fc71d0a53393232
David García <jgarcia3@unisangil.edu.co>
2024-09-13 16:01:01 -0500

    estrustura if-elif-else

---
+++ estructura_if.py
@@ -0,0 +1,19 @@
+#Variable
+numero = 2
+#if
+if numero == 10:
+    print("numero 10")
+#if-else
+if numero == 10:
+    print("numero 10")
+else:
+    print("sigue intentando")
+#elif
+if numero == 10:
+    print("numero 10")
+elif numero == 1:
+    print("numero 1")
+elif numero == 2:
+    print("numero 2")
+else:
+    print("sigue intentando")
