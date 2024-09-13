40e4c6820a1ec1ebc2535bd310b04f0855db4bd4
David García <jgarcia3@unisangil.edu.co>
2024-09-13 17:37:06 -0500

    ejemplo calculadora estructura de repetición while

--- ejemplo_while.py
+++ ejemplo_while.py
@@ -1,18 +1,48 @@
-#Estructura de repeticion
-opcion = input("Digite la opcion del menu: ")
-resultado = 1+2
-while opcion != "salir":
-    if opcion == "1":
-      print(f"suma= {resultado}")
-    elif opcion == "2":
-      print("resta=", 3-4)
-    elif opcion == "3":
-      print("multiplicacion=", 3*4)
-    elif opcion == "4":
-      print("div=", 3/4)
-    elif opcion == "5":
-      print("potencia=", 3**4)
-    else:
-      print("Opcion incorrecta, ingrese del 1 al 5")
-print("Fuera del menu")      
+#variable
+opcion = 0
+#Estructura repeticion
+while opcion != 6:
+    #Menu calculadora
+    print("\nHola Calculadora" )
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. Division")
+    print("5. Potencia")
+    print("6. Salir")
+    #opcion a ingresar
+    try:
+        opcion = int(input("Digite la opcion de la calculadora: "))
+        if opcion == 1:
+            num1 = int(input("Digite el primer numero: "))
+            num2 = int(input("Digite el segundo numero: "))
+            resultado = num1 + num2
+            print(f"Suma = {resultado}")
+        elif opcion == 2:
+            num1 = int(input("Digite el primer numero: "))
+            num2 = int(input("Digite el segundo numero: "))
+            resultado = num1 - num2
+            print(f"Resta = {resultado}")
+        elif opcion == 3:
+            num1 = int(input("Digite el primer numero: "))
+            num2 = int(input("Digite el segundo numero: "))
+            resultado = num1 * num2
+            print(f"Multiplicación = {resultado}")
+        elif opcion == 4:
+            num1 = int(input("Digite el primer numero: "))
+            num2 = int(input("Digite el segundo numero: "))
+            resultado = num1 / num2
+            print(f"Division = {resultado}")    
+        elif opcion == 5:
+            base = int(input("Digite el primer numero: "))
+            potencia = int(input("Digite el segundo numero: "))
+            resultado = base ** potencia
+            print(f"Potencia = {resultado}")
+        elif opcion == 6:
+            print(f"Saliendo de la calculadora...")
+        else:
+            print("Error")
+    except ValueError:
+        print("Ingrese una opcion valida")            
+print("Gracias por utilizar la calculadora")      
 
